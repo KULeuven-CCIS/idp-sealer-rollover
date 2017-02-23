dbdb01b9991dde9aaed0a76702f5b2cb3df6eaf7
nxadm <pub.claudio@gmail.com>
2017-02-23 12:27:09 +0100

    add dep

--- utils/create_fatpacked_bin.sh
+++ utils/create_fatpacked_bin.sh
@@ -2,6 +2,10 @@
 set -xv
 # Create a fatcked standaline executable
 # You should be in the bin directory of the distribution
+if [ ! -z `which cpan` ]; then
+    cpanm YAML::Tiny
+    else cpan YAML::Tiny
+fi
 APP=../idp-sealer-rollover
 fatpack trace ../src/idp-sealer-rollover.pl
 fatpack packlists-for `cat fatpacker.trace` >packlists
